6d6a1b483ed923b0b98f90016cc11be337bdb87a
popovanton <mr.anpopov@mail.ru>
2022-06-10 01:09:55 +0300

    embedded server removed

--- src/main/java/ru/anpopov/privatevpn/PrivateVpnApplication.java
+++ src/main/java/ru/anpopov/privatevpn/PrivateVpnApplication.java
@@ -2,6 +2,7 @@ package ru.anpopov.privatevpn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
 @SpringBootApplication
 public class PrivateVpnApplication {
--- src/main/java/ru/anpopov/privatevpn/config/UdpConfig.java
+++ src/main/java/ru/anpopov/privatevpn/config/UdpConfig.java
@@ -1,26 +1,26 @@
 package ru.anpopov.privatevpn.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.ip.udp.UnicastReceivingChannelAdapter;
 
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class UdpConfig {
 
     @Bean
-    public IntegrationFlow processUniCastUdpMessageHttps() {
+    public IntegrationFlow processUniCastUdpMessageHttps(Environment environment) {
+        int port = Integer.parseInt(environment.getProperty("server.port"));
+        log.info("UDP is on {}", port);
         return IntegrationFlows
-                .from(new UnicastReceivingChannelAdapter(443))
-                .handle("udpService", "handleMessage")
-                .get();
-    }
-
-    @Bean
-    public IntegrationFlow processUniCastUdpMessage() {
-        return IntegrationFlows
-                .from(new UnicastReceivingChannelAdapter(80))
+                .from(new UnicastReceivingChannelAdapter(port))
                 .handle("udpService", "handleMessage")
                 .get();
     }
