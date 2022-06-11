88d51cce2a5ecadb84be87e9ce0cf471f1a88f52
popovanton <mr.anpopov@mail.ru>
2022-06-11 15:23:18 +0300

    IPSec IKEv2 ports are listened async

--- src/main/java/ru/anpopov/privatevpn/PrivateVpnApplication.java
+++ src/main/java/ru/anpopov/privatevpn/PrivateVpnApplication.java
@@ -3,8 +3,10 @@ package ru.anpopov.privatevpn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PrivateVpnApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ru/anpopov/privatevpn/config/UdpConfig.java
+++ src/main/java/ru/anpopov/privatevpn/config/UdpConfig.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.Async;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -13,13 +13,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Objects;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class UdpConfig {
 
+    @Async
     @EventListener(ApplicationReadyEvent.class)
     public void steady() {
         try {
@@ -44,29 +44,28 @@ public class UdpConfig {
         }
     }
 
+    @Async
     @EventListener(ApplicationReadyEvent.class)
-    public void ready(Environment environment) {
-        if (Objects.nonNull(environment.getProperty("server.port"))) {
-            try {
-                int count = 0;
-                while (true) {
-                    System.out.println("doing things... on 4500" + count++);
-                    try (ServerSocket serverSocket = new ServerSocket(4500);
-                         Socket accept = serverSocket.accept()
-                    ) {
-                        try (InputStream inputStream = accept.getInputStream();
-                             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                             OutputStream outputStream = accept.getOutputStream();
-                             BufferedReader br = new BufferedReader(inputStreamReader)) {
-                            br.lines().forEach(System.out::println);
-                        }
-                    } catch (Exception e) {
-                        System.out.println(e.toString());
+    public void ready() {
+        try {
+            int count = 0;
+            while (true) {
+                System.out.println("doing things... on 4500" + count++);
+                try (ServerSocket serverSocket = new ServerSocket(4500);
+                     Socket accept = serverSocket.accept()
+                ) {
+                    try (InputStream inputStream = accept.getInputStream();
+                         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                         OutputStream outputStream = accept.getOutputStream();
+                         BufferedReader br = new BufferedReader(inputStreamReader)) {
+                        br.lines().forEach(System.out::println);
                     }
+                } catch (Exception e) {
+                    System.out.println(e.toString());
                 }
-            } catch (Exception e) {
-                log.info("Possibly stopping. Good bye, world!");
             }
+        } catch (Exception e) {
+            log.info("Possibly stopping. Good bye, world!");
         }
     }
 }
