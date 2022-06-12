29c227eb011d91118eb9754d24af06cf9ef03147
popovanton <mr.anpopov@mail.ru>
2022-06-12 03:21:46 +0300

    UDP listening configured; SPI parsed; apache commons codec lib added for hex conversions

--- src/main/java/ru/anpopov/privatevpn/config/UdpConfig.java
+++ src/main/java/ru/anpopov/privatevpn/config/UdpConfig.java
@@ -2,70 +2,44 @@ package ru.anpopov.privatevpn.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.StringJoiner;
 
 @Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class UdpConfig {
-
-    @Async
-    @EventListener(ApplicationReadyEvent.class)
-    public void steady() {
-        try {
-            int count = 0;
-            while (true) {
-                System.out.println("doing things... on 500" + count++);
-                try (ServerSocket serverSocket = new ServerSocket(500);
-                     Socket accept = serverSocket.accept()
-                ) {
-                    try (InputStream inputStream = accept.getInputStream();
-                         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                         OutputStream outputStream = accept.getOutputStream();
-                         BufferedReader br = new BufferedReader(inputStreamReader)) {
-                        br.lines().forEach(System.out::println);
-                    }
-                } catch (Exception e) {
-                    System.out.println(e.toString());
-                }
-            }
-        } catch (Exception e) {
-            log.info("Possibly stopping. Good bye, world!");
-        }
-    }
-
     @Async
     @EventListener(ApplicationReadyEvent.class)
     public void ready() {
-        try {
+        try (DatagramSocket serverSocket = new DatagramSocket(500)) {
             int count = 0;
+            byte[] in = new byte[1024];
             while (true) {
-                System.out.println("doing things... on 4500" + count++);
-                try (ServerSocket serverSocket = new ServerSocket(4500);
-                     Socket accept = serverSocket.accept()
-                ) {
-                    try (InputStream inputStream = accept.getInputStream();
-                         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                         OutputStream outputStream = accept.getOutputStream();
-                         BufferedReader br = new BufferedReader(inputStreamReader)) {
-                        br.lines().forEach(System.out::println);
-                    }
-                } catch (Exception e) {
-                    System.out.println(e.toString());
-                }
+                System.out.println("doing things... on 500 " + count++);
+                DatagramPacket packetIn = new DatagramPacket(in, in.length);
+                serverSocket.receive(packetIn);
+                byte[] data = packetIn.getData();
+
+                //actual payload starts on 42 in wireshark
+                String spiInitiator = Hex.encodeHexString(Arrays.copyOfRange(data, 0, 8));
+                String spiResponse = Hex.encodeHexString(Arrays.copyOfRange(data, 8, 16));
+
+                System.out.println("so the initiator is " + spiInitiator);
+                System.out.println("so the response is " + spiResponse);
             }
         } catch (Exception e) {
             log.info("Possibly stopping. Good bye, world!");
         }
     }
+
 }
